c9723106f6d8111563c9415a70e9674bb8347a08
Serigne Bassirou NDIAYE <dstndiaye@gmail.com>
2025-03-14 22:50:00 +0100

    Création du Repository Produit

---
+++ src/main/java/com/microservicesProjet/service_produit/repository/ProduitRepository.java
@@ -0,0 +1,10 @@
+package com.microservicesProjet.service_produit.repository;
+
+import com.microservicesProjet.service_produit.model.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+    
+}
