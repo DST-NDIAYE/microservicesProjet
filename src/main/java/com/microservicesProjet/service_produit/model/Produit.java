699eb2792d5f6ded8e2066314fe721607abaf9f0
Serigne Bassirou NDIAYE <dstndiaye@gmail.com>
2025-03-15 07:20:47 +0100

    Mise a jour de Produit.java et pruidutRepository pour remplacer Le passage de javax.persistence à jakarta.persistence

--- src/main/java/com/microservicesProjet/service_produit/model/Produit.java
+++ src/main/java/com/microservicesProjet/service_produit/model/Produit.java
@@ -1,29 +1,29 @@
 package com.microservicesProjet.service_produit.model;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Table(name = "produits")
 public class Produit {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String nom;
-    private String description;
-    private Double prix;
-    private Integer quantite;
 
-    // Vous pouvez ajouter d'autres attributs si nécessaire (par exemple, la catégorie, etc.)
+    @Column(nullable = false)
+    private double prix;
+
+    @Column(nullable = false)
+    private int quantite;
+
+    private String description;
 }
