26d723907282deadf78e6a884ecd0db6f3c3e4dc
Serigne Bassirou NDIAYE <dstndiaye@gmail.com>
2025-03-16 20:18:54 +0100

    debut du microservice utilisateur

--- src/main/java/com/microservicesProjet/ServiceProduitApplication.java
+++ src/main/java/com/microservicesProjet/ServiceProduitApplication.java
@@ -1,4 +1,4 @@
-package com.microservicesProjet.service_produit;
+package com.microservicesProjet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
